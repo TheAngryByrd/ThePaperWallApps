8f61c324c3ed02393c3417b747d1262a33be69f6
Jimmy Byrd <jimmybyrd@gmail.com>
2013-10-14 04:07:10 -0400

    Cleanup

--- src/ThePaperWall.Core/Models/Themes.cs
+++ src/ThePaperWall.Core/Models/Themes.cs
@@ -7,25 +7,14 @@ namespace ThePaperWall.Core.Models
     {
         public Themes(IEnumerable<Theme> all)
         {
-            _all = all;
+            _all = all.ToList();
         }
-        private IEnumerable<Theme> _all;
+        private IList<Theme> _all;
 
-        public IEnumerable<Theme> All
-        {
-            get
-            {
-                return _all;
-            }
-            
-        }
+        public IEnumerable<Theme> All { get { return _all; } }
 
-        public Theme WallPaperOfTheDay
-            {
-                get
-                {
-                    return _all.First(t => t.Name == "Wallpaper of the Day");
-                }                
-            }
+        public Theme WallPaperOfTheDay { get { return _all[0]; } }
+        public Theme Top4 { get { return _all[1]; } }
+        public Theme Recent50 { get { return _all[2]; } }   
     }
 }
