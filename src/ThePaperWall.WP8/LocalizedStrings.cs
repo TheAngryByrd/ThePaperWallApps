812110bd63f004302007432f137ca8f8f04b29cf
Jimmy Byrd <jimmybyrd@gmail.com>
2013-10-14 02:28:50 -0400

    Added WP8

---
+++ src/ThePaperWall.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ThePaperWall.WP8.Resources;
+
+namespace ThePaperWall.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
