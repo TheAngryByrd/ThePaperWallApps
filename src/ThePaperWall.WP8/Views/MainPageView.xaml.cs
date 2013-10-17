ec45061ec83f03925f7a945e9259e14a920af7e5
Jimmy Byrd <jimmybyrd@gmail.com>
2013-10-16 21:22:09 -0400

    Initial wp8 code

---
+++ src/ThePaperWall.WP8/Views/MainPageView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace ThePaperWall.WP8.Views
+{
+    public partial class MainPageView : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPageView()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
