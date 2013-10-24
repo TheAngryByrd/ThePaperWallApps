84cf91a02b306dbf0b2256c3a6d8aa4c6cb35128
Jimmy Byrd <jimmybyrd@gmail.com>
2013-10-24 18:47:28 -0400

    Refactoer

--- src/ThePaperWall.WP8/ViewModels/CategoryItem.cs
+++ src/ThePaperWall.WP8/ViewModels/CategoryItem.cs
@@ -1,27 +1,58 @@
-﻿using ReactiveUI;
+﻿using Caliburn.Micro;
+using ReactiveUI;
+using Splat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace ThePaperWall.WP8.ViewModels
 {
     public class CategoryItem : ReactiveObject, IComparable<CategoryItem>
     {
+        public CategoryItem(string id, string name, Func<Task<IBitmap>> lazyImageFactory = null)
+        {
+            Id = id;
+            Name = name;
+            _lazyImageFactory = lazyImageFactory;
+        }
+
+        public async Task LoadImage()
+        {
+            if (_lazyImageFactory != null)
+            {
+                var image = await _lazyImageFactory();
+                await Execute.OnUIThreadAsync(() => ImagePath = image.ToNative());
+            }
+         
+        }
+
         public string Id { get; private set; }
 
         public string Name { get; set; }
 
+        private readonly Func<Task<IBitmap>> _lazyImageFactory;
 
-        public CategoryItem(string id, string name)
+        public ImageSource _imagePath;
+
+        public ImageSource ImagePath
         {
-            // TODO: Complete member initialization
-            Id = id;
-            Name = name;
+            get
+            {
+                return _imagePath;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _imagePath, value);
+            }
         }
+
         public int CompareTo(CategoryItem other)
         {
             return string.Compare(this.Name, other.Name);
         }
+
     }
 }
