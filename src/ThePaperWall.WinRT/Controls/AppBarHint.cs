a316c494570e76a06af600cf4220c7e322ee6666
Jimmy Byrd <jimmybyrd@gmail.com>
2014-01-23 19:26:44 -0500

    Added appbar ellipses

---
+++ src/ThePaperWall.WinRT/Controls/AppBarHint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace ThePaperWall.WinRT.Controls
+{
+    public class AppBarHint : Button
+    {
+        protected override void OnTapped(TappedRoutedEventArgs e)
+        {
+            base.OnTapped(e);
+
+            if (Window.Current.Content is Frame)
+            {
+                var frame = (Frame)Window.Current.Content;
+                if (frame.Content is Page)
+                {
+                    var page = (Page)frame.Content;
+                    if (page.BottomAppBar != null)
+                    {
+                        page.BottomAppBar.IsOpen = true;
+                    }
+                }
+            }
+        }
+    }
+}
